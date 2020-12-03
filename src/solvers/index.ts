4e7010368deda8989d82076fa77a74ffb643398b
Charles Rollin <charles.rollin@illuin.tech>
2020-12-03 09:43:20 +0100

    feat: solve day 3

--- src/solvers/index.ts
+++ src/solvers/index.ts
@@ -1,8 +1,9 @@
 import { day1 } from "./1";
 import { day2 } from "./2";
+import { day3 } from "./3";
 import { Solver } from "./Solver";
 
-const solversArray = [day1, day2];
+const solversArray = [day1, day2, day3];
 export const Solvers: { [key: number]: Solver<any, any> } = {};
 
 solversArray.forEach(solver => {
