059de5d447c2f30b2be3c4c4156f2f634f1ade0d
Charles Rollin <charles.rollin@illuin.tech>
2020-12-12 12:19:59 +0100

    feat: specify which day to solve in CLI

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+
+const args = process.argv.slice(2);
+
 async function solve() {
-  const DAY = 12;
+  let DAY = parseInt(args[0], 10);
+  if (isNaN(DAY)) {
+    const days = fs
+      .readdirSync(`${__dirname}`)
+      .map(name => name.match(/^day(\d*)$/))
+      .filter(match => match !== null)
+      .map(match => parseInt(match[1], 10));
+    DAY = Math.max(...days);
+  }
   const { solver } = await import(`./day${DAY}`);
 
   console.log(solver.solveFirstPart());
