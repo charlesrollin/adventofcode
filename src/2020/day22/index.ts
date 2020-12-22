83ddb6eb2ae86444414ddc199bb986875410da4e
Charles Rollin <charles.rollin@illuin.tech>
2020-12-22 17:04:42 +0100

    feat: solve day 22

---
+++ src/2020/day22/index.ts
@@ -0,0 +1,71 @@
+import { Solver } from '../../shared/Solver';
+
+class DaySolver extends Solver<number[][], number> {
+  constructor() {
+    super(__dirname);
+  }
+
+  protected parseInput = (input: string) => {
+    return input.split('\n\n').map(line => {
+      const [rawPlayer, ...rawCards] = line.split('\n');
+      return rawCards.map(raw => parseInt(raw));
+    });
+  };
+
+  private getScore(deck: number[]) {
+    return deck.reverse().reduce((acc, curr, idx) => acc + (idx + 1) * curr, 0);
+  }
+
+  protected _solveFirstPart = (decks: number[][]) => {
+    while (decks.every(deck => deck.length > 0)) {
+      const playedCards = decks.map(deck => deck.shift());
+      const winner = playedCards[0] > playedCards[1] ? 0 : 1;
+      if (winner === 0) {
+        decks[0].push(...playedCards);
+      } else {
+        decks[1].push(...playedCards.reverse());
+      }
+    }
+    let winningDeck = decks[0].length !== 0 ? decks[0] : decks[1];
+    return this.getScore(winningDeck);
+  };
+
+  private recursiveCombat(decks: number[][]) {
+    const existingConfigurations: Set<string> = new Set();
+    while (decks.every(deck => deck.length !== 0)) {
+      if (existingConfigurations.has(decks.map(deck => deck.join()).join('|'))) {
+        return {
+          winningDeck: decks[0],
+          winner: 0,
+        };
+      } else {
+        existingConfigurations.add(decks.map(deck => deck.join()).join('|'));
+      }
+      const revealedCards = decks.map(deck => deck.shift());
+      if (revealedCards.every((card, idx) => card <= decks[idx].length)) {
+        let { winner } = this.recursiveCombat(
+          revealedCards.map((card, idx) => decks[idx].slice(0, card))
+        );
+        decks[winner].push(...(winner === 0 ? revealedCards : revealedCards.reverse()));
+      } else {
+        if (revealedCards[0] > revealedCards[1]) {
+          decks[0].push(...revealedCards);
+        } else {
+          decks[1].push(...revealedCards.reverse());
+        }
+      }
+    }
+    const winner = decks[0].length !== 0 ? 0 : 1;
+    return {
+      winner,
+      winningDeck: decks[winner],
+    };
+  }
+
+  protected _solveSecondPart = (input: number[][]) => {
+    const { winningDeck } = this.recursiveCombat(input);
+    return this.getScore(winningDeck);
+  };
+}
+
+export const solver = new DaySolver();
---
+++ src/2020/day22/solver.test.ts
@@ -0,0 +1,60 @@
+import { runCases, TestCase } from '../../shared/tests';
+import { solver } from '.';
+
+const testCases: TestCase[] = [
+  {
+    input: `Player 1:
+    9
+    2
+    6
+    3
+    1
+
+    Player 2:
+    5
+    8
+    4
+    7
+    10`.replace(/  /g, ''),
+    expected: {
+      part1: 306,
+      part2: 291,
+    },
+  },
+  {
+    input: `Player 1:
+    5
+    8
+    4
+    7
+    10
+
+    Player 2:
+    9
+    2
+    6
+    3
+    1`.replace(/  /g, ''),
+    expected: {
+      part1: 306,
+      part2: 291,
+    },
+  },
+  {
+    title: 'small looping game',
+    input: `Player 1:
+    43
+    19
+
+    Player 2:
+    2
+    29
+    14`.replace(/  /g, ''),
+    expected: {
+      part2: 105,
+    },
+  },
+];
+describe(solver.dirName.split('/').pop(), () => {
+  runCases(solver, testCases);
+});
