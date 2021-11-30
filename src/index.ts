8b99b8c44c88a9f3c61327e2d1fcfd4d1bdaef32
Charles Rollin <charles.rollin@illuin.tech>
2021-11-30 15:02:24 +0100

    refactor: use highest year if none is specified

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,31 @@ import * as fs from 'fs';
 const args = process.argv.slice(2);
 
 async function solve() {
-  let DAY = parseInt(args[0], 10);
+  let YEAR: number;
+  let DAY: number;
+  if (args.length > 1) {
+    YEAR = parseInt(args[0], 10);
+    DAY = parseInt(args[1], 10)
+  } else {
+    DAY = parseInt(args[0], 10);
+  }
+  if (isNaN(YEAR)) {
+    const years = fs
+      .readdirSync(`${__dirname}`)
+      .map(name => name.match(/^(\d*)$/))
+      .filter(match => match !== null)
+      .map(match => parseInt(match[1], 10));
+      YEAR = Math.max(...years);
+  }
   if (isNaN(DAY)) {
     const days = fs
-      .readdirSync(`${__dirname}/2020`)
+      .readdirSync(`${__dirname}/${YEAR}`)
       .map(name => name.match(/^day(\d*)$/))
       .filter(match => match !== null)
       .map(match => parseInt(match[1], 10));
     DAY = Math.max(...days);
   }
-  const { solver } = await import(`./2020/day${DAY}`);
+  const { solver } = await import(`./${YEAR}/day${DAY}`);
 
   console.log(solver.solveFirstPart());
   console.log(solver.solveSecondPart());
