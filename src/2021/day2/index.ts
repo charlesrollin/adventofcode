0b7e19c66379a97709a96d104baf4dc81da335e3
Charles Rollin <charles.rollin@illuin.tech>
2021-12-02 07:38:20 +0100

    feat(2021): solve day 2

---
+++ src/2021/day2/index.ts
@@ -0,0 +1,56 @@
+import { Solver } from './../../shared/Solver';
+
+class DaySolver extends Solver<{ direction: string; distance: number }[], number> {
+  constructor() {
+    super(__dirname);
+  }
+
+  protected parseInput = (input: string) => {
+    return input
+      .split(/\r?\n/)
+      .map(item => item.split(' '))
+      .map(([direction, distance]) => ({ direction, distance: parseInt(distance, 10) }));
+  };
+
+  protected _solveFirstPart = (input: { direction: string; distance: number }[]) => {
+    let horizontalPosition = 0;
+    let depth = 0;
+    input.forEach(({ direction, distance }) => {
+      switch (direction) {
+        case 'forward':
+          horizontalPosition += distance;
+          break;
+        case 'up':
+          depth -= distance;
+          break;
+        case 'down':
+          depth += distance;
+          break;
+      }
+    });
+    return horizontalPosition * depth;
+  };
+
+  protected _solveSecondPart = (input: { direction: string; distance: number }[]) => {
+    let horizontalPosition = 0;
+    let depth = 0;
+    let aim = 0;
+    input.forEach(({ direction, distance }) => {
+      switch (direction) {
+        case 'forward':
+          horizontalPosition += distance;
+          depth += aim * distance;
+          break;
+        case 'up':
+          aim -= distance;
+          break;
+        case 'down':
+          aim += distance;
+          break;
+      }
+    });
+    return horizontalPosition * depth;
+  };
+}
+
+export const solver = new DaySolver();
---
+++ src/2021/day2/solver.test.ts
@@ -0,0 +1,16 @@
+import { runCases, TestCase } from './../../shared/tests';
+import { solver } from '.';
+
+const testCases: TestCase[] = [
+  {
+    input: `forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2`,
+    expected: {
+      part1: 150,
+      part2: 900,
+    },
+  },
+];
+
+describe(solver.dirName.split('/').pop(), () => {
+  runCases(solver, testCases);
+});
