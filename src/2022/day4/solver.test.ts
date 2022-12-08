7570582b11d33c5a44c134064b6598f5a0c4c3c3
Charles Rollin <10196757+charlesrollin@users.noreply.github.com>
2022-12-08 07:49:43 +0100

    test(2022): wrong test data

--- src/2022/day4/solver.test.ts
+++ src/2022/day4/solver.test.ts
@@ -3,7 +3,7 @@ import { solver } from '.';
 
 const testCases: TestCase[] = [
   {
-    input: `2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8\n24-47,23-66\n43-48,29-49`,
+    input: `2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8`,
     expected: {
       part1: 2,
       part2: 4,
